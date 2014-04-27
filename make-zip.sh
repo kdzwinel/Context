6896481d71796947f2bc38ec61728c902426a250
Konrad Dzwinel <kdzwinel@gmail.com>
2014-04-27 14:02:57 +0200

    Adding grunt support.

--- make-zip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r Context _locales/de _locales/en _locales/es _locales/fr _locales/it _locales/ja _locales/pl _locales/pt_BR _locales/zh_CN css icons js screenshots other background.html manifest.json notification.html options.html popup.html
